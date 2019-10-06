f3446374d7191a3d95327985388643b2efbff897
Jianzhou_D <su.jian@husky.neu.edu>
2019-10-06 09:13:30 -0400

    testing github

--- brains/string_sub.py
+++ brains/string_sub.py
@@ -7,7 +7,10 @@ class SStr(str):
                 pass
             else:
                 nl_dis.append(i)
+        return nl_dis
 
 
 if __name__ == '__main__':
-    str1 = SStr('abc')
+    str1 = SStr('abcde')
+    str2 = SStr('abddd')
+    print(str1-str2)
