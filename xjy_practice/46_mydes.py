69e9800920bc1d6e47511544d5ee87bd24835f8e
Jianzhou Su <todo@todo.com>
2019-10-06 21:53:11 -0400

    update

--- xjy_practice/46_mydes.py
+++ xjy_practice/46_mydes.py
@@ -8,14 +8,26 @@ class Record:
         self.filename = 'record.txt'
 
     def __get__(self, instance, owner):
-        with open(self.filename, 'a', encoding='urf-8') as f:
-            f.write('{}{}{}{}{}'.format(self.name, time.ctime(), self.name, 'read as ', str(self.val)))
+        with open(self.filename, 'a', encoding='utf-8') as f:
+            f.write('\n{} {} {} {} {}'.format(self.name, time.ctime(), self.name, 'read as ', str(self.val)))
 
         return self.val
 
     def __set__(self, instance, value):
         filename = '%s_record.txt' % self.name
         with open(self.filename, 'a', encoding='utf-8') as f:
-            f.write('{}{}{}{}{}'.format(self.name, time.ctime(), self.name, 'set as ', str(value)))
+            f.write('\n{} {} {} {} {}'.format(self.name, time.ctime(), self.name, 'set as ', str(value)))
 
         self.val = value
+
+
+class Test:
+    a = Record(1, 'rec')
+    x = a
+
+
+c = Test()
+print(c.x)
+print(c.a)
+c.x = 1000
+print(c.x)
