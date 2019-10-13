51d78f403c2903402c46a5b2729161795e9ca8ff
jianzhou <sujianzhou@outlook.com>
2019-10-13 11:15:14 -0400

    two sum testing

---
+++ brains/two_sum.py
@@ -0,0 +1,13 @@
+def twoSum(nums, target):
+    get_key = {}
+    for i in range(len(nums)):
+        another_num = target - nums[i]
+        if another_num in get_key:
+            return get_key[another_num], i
+        if nums[i] not in get_key:
+            get_key[nums[i]] = i
+
+
+
+
+a = twoSum([2, 7, 14, 15], 17)
